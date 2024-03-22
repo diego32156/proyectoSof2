c728a1356bbba5139462a31a7ff4556e2ee73d79
diego32156 <diegotafurborja@gmail.com>
2024-03-21 21:04:50 -0500

    dtafurb-> Commit inicial de Front proyecto Software2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { BancoComponent } from './banco/banco.component';
+import { CitaComponent } from './cita/cita.component';
+import { TipoServicioComponent } from './tipo-servicio/tipo-servicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    EmpleadosComponent,
+    BancoComponent,
+    CitaComponent,
+    TipoServicioComponent
   ],
   imports: [
     BrowserModule,
